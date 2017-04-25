21a0f9b7fc8aa765f9ea00b2427b73b4ca59f75a
Romain Roffé <rofferom@gmail.com>
2017-04-25 21:50:11 +0200

    Fix folder month
    According to java doc:
        "The first month of the year is JANUARY which is 0;
        the last depends on the number of months in a year"

--- app/src/main/java/fr/rtwo/gpstracker/logs/Tools.java
+++ app/src/main/java/fr/rtwo/gpstracker/logs/Tools.java
@@ -33,7 +33,7 @@ public class Tools {
         String folderName = String.format(
                 "%04d%02d%02d-%02d%02d%02d",
                 c.get(Calendar.YEAR),
-                c.get(Calendar.MONTH),
+                c.get(Calendar.MONTH) + 1,
                 c.get(Calendar.DAY_OF_MONTH),
                 c.get(Calendar.HOUR_OF_DAY),
                 c.get(Calendar.MINUTE),
