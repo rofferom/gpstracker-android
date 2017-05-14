0a0ae2fb2ec8fabcb382b432ab718c4438d01dc3
Romain Roffé <rofferom@gmail.com>
2017-05-14 09:58:02 +0200

    tools/genhtml: add merge option to generate only one output file

--- tools/genhtml.py
+++ tools/genhtml.py
@@ -42,10 +42,42 @@ def convertFile(srcPath):
 	else:
 		print("Empty file, skipped")
 
+def convertFiles(files):
+	for path in files:
+		convertFile(path)
+
+def mergeFiles(files, outPath):
+	locationList = []
+
+	for path in files:
+		print("Parsing file %s" % path)
+		locationList.extend(getLocationList(path))
+
+	if len(locationList) > 0:
+		print("Generate %s" % outPath)
+		generateHtml(locationList, outPath)
+	else:
+		print("No locations found")
+
+def convertFileList(args):
+	if args.merge:
+		if not args.output:
+			print("Missing output path")
+			return False
+
+		mergeFiles(args.files, args.output)
+	else:
+		convertFiles(args.files)
+
+	return True
+
 def parseArgs():
 	parser = argparse.ArgumentParser(description='Generate html files from GpsTracker record files.')
 
 	parser.add_argument("-f", "--files", nargs="+", help="List of files to parse")
+
+	parser.add_argument('-m', '--merge', action='store_true', help='Merge inputs to generate only one output file')
+	parser.add_argument("-o", "--output", help="File to generate")
 
 	return (parser, parser.parse_args())
 
@@ -54,8 +86,9 @@ if __name__ == '__main__':
 
 	# Parse a file list
 	if args.files:
-		for path in args.files:
-			convertFile(path)
+		ret = convertFileList(args)
+		if not ret:
+			argParser.print_help()
 	else:
 		argParser.print_help()
 
